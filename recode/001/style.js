deac5f6e16a1603f714d05b48245f3d9b62d626e
Anderson <marquesandersonds@gmail.com>
2022-04-07 08:44:46 -0300

    Novos

---
+++ recode/001/style.js
@@ -0,0 +1,13 @@
+function funcao1(){
+document.write('Esse texto deve aparecer após o click.');
+}
+alert("Seja Bem-vindo ao Site!")
+
+/* let nome=prompt("Qual seu nome?")
+    document.write(nome + ", Seja Bem-vindo ao site!") */
+
+/* let opcao=confirm("Deseja sair da pagina?")
+document.write("Sua escolha," + opcao); */
+
+let valor=prompt ("Qual o seu nome?")
+alert("Olá," + (valor));
---
+++ recode/002/style.js
@@ -0,0 +1,7 @@
+let tabuada=7
+for (let contador = 1; contador <=10; contador++){
+console.log(
+    tabuada,'x', contador, '=',
+    tabuada * contador 
+);
+}
