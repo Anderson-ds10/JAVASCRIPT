178f05c90716ac03dd5011c31cc98075674c20dc
Anderson <marquesandersonds@gmail.com>
2022-07-15 11:49:33 -0300

    Estruturas de repetição

---
+++ cursoemvideo/ex14.js
@@ -0,0 +1,26 @@
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
+
+/*
+var c = 1
+while(c <= 25) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+}
+while(c <= 25) {
+}
