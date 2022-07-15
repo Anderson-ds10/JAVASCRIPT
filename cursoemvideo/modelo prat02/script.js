5abb19cea077fc26c119d36755e4bae74c599f09
Anderson <marquesandersonds@gmail.com>
2022-07-15 10:21:24 -0300

    Site finalizado

--- cursoemvideo/modelo prat02/script.js
+++ cursoemvideo/modelo prat02/script.js
@@ -1,1 +1,50 @@
-alert('Olá')
+function Verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fAno = document.getElementById('txtano')
+    var res = document.getElementById('res')
+    if(fAno.value.length == 0 || fAno.value > ano){
+        window.alert('[ERRO] Verificar os dados e tente novamente')
+    }else {
+        var fsex = document.getElementsByName('radiosex')
+        var idade = ano - Number(fAno.value)
+        // res.innerHTML = `Idade calculada ${idade}` TESTE
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if(fsex[0].checked){
+            genero = 'Homem'
+            if(idade >= 0 && idade < 10){
+                //Criança
+                img.setAttribute('src', 'criancamasc.png')
+            }else if (idade < 21){
+                //Jovem
+                img.setAttribute('src', 'adolescentemasc.png')
+            }else if (idade < 50){
+                //Adulto
+                img.setAttribute('src', 'adultomasc.png')
+            }else {
+                //Idoso
+                img.setAttribute('src', 'velhomasc.png')
+            }
+        } else if(fsex[1].checked){
+            genero = 'Mulher'
+            if(idade >= 0 && idade < 10){
+                //Criança
+                img.setAttribute('src', 'criancafemi.png')
+            }else if (idade < 21){
+                //Jovem
+                img.setAttribute('src', 'adolescentefemi.png')
+            }else if (idade < 50){
+                //Adulto
+                img.setAttribute('src', 'adultofemi.png')
+            }else {
+                //Idoso
+                img.setAttribute('src', 'velhafemi.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+}
